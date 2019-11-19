3d15e0fe6311131a4aeb69c10d054eac96befc60
Bill Gates <coollikeacheetah@yahoo.com>
2019-11-18 22:09:33 -0600

    played with schedule and threading

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Install Python3 dependencies
 $ pip3 install paho.mqtt
 $ pip3 install configparser
+$ pip3 install schedule
--- powerhouse.py
+++ powerhouse.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 import configparser
 import time
+import schedule
+import threading
 import os
 from gpiozero import LED
 from time import sleep
@@ -30,6 +32,19 @@ pin = config.getint('gpio', 'pin')
 print("initializing pin...")
 switch = LED(pin)
 
+# TEMP
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+def scheduler_thread():
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+def test():
+    print("test")
+schedule.every(3).seconds.do(test).tag("status")
+run_threaded(scheduler_thread)
+
 # The callback for when the client receives a CONNACK response from the server
 def on_connect(client, userdata, flags, rc):
     print("connected  : topic="+topic_control+" : status="+str(rc))
@@ -38,32 +53,51 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     client.subscribe(topic_control)
 
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print("received : topic=%s : payload=%s" % (msg.topic, msg.payload))
     if msg.payload.decode('UTF-8') == '0':
-        toggleOn()
-    elif msg.payload.decode('UTF-8') == '1':
         toggleOff()
+    elif msg.payload.decode('UTF-8') == '1':
+        toggleOn()
+
+# callback when disconnect from MQTT server
+def on_disconnect():
+    print("disconnected")
 
 print("initializing mqtt...")
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_disconnect = on_disconnect
 
 # toggle on
 def toggleOn():
     print("toggleOn")
     switch.on()
+    global power_house_status
     power_house_status = True
-    client.publish(topic_status, 'on')
+    publishStatus()
 
 # toggle off
 def toggleOff():
     print("toggleOff")
     switch.off()
+    global power_house_status
     power_house_status = False
-    client.publish(topic_status, 'off')
+    publishStatus()
+
+# publish status
+def publishStatus():
+    print("publishStatus: "+str(power_house_status))
+    if power_house_status:
+        client.publish(topic_status, 'on')
+    else:
+        client.publish(topic_status, 'off')
+
+
+
 
 # toggle the switch off
 toggleOff()
